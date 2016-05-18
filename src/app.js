b04c0b10049f23b3646beafe48b0e8ee33cc4422
Paul Tichonczuk <pault@debian>
2016-05-18 03:24:13 -0400

    tern config navigation

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import route from "can/route/";
 import 'can/map/define/';
 import 'can/route/pushstate/';
 
+route(':page', {page: 'home'});
+
 const AppViewModel = Map.extend({
   define: {
     message: {
