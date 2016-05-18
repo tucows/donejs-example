6ba72b054515e35140b861f812d82827d0d04746
Paul Tichonczuk <pault@debian>
2016-05-18 05:48:27 -0400

    simple ajax call working with faked API

--- src/simple-ajax/simple-ajax.js
+++ src/simple-ajax/simple-ajax.js
@@ -3,13 +3,22 @@ import Map from 'can/map/';
 import 'can/map/define/';
 import './simple-ajax.less!';
 import template from './simple-ajax.stache!';
+import Fixture from 'can/util/fixture/';
 
 export const ViewModel = Map.extend({
   define: {
-    message: {
-      value: 'This is the simple-ajax component'
+    stuff: {
+			value() {
+				return can.ajax("/api/simple-ajax");
+			}
     }
   }
+});
+
+Fixture("GET /api/simple-ajax", function() {
+	return {
+		message: "The simple ajax call returned this."
+	}
 });
 
 export default Component.extend({
