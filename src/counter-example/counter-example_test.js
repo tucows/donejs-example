01e6f95b2ef1fc2489477c34e142b70846b06eef
Paul Tichonczuk <pault@tucows.com>
2016-09-30 14:45:59 -0400

    removing counter

--- src/counter-example/counter-example_test.js
+++
@@ -1,9 +0,0 @@
-import { ViewModel } from './counter-example';
-
-// ViewModel unit tests
-describe('donejs-example/counter-example', function(){
-  it('Has message', function(){
-    var vm = new ViewModel();
-    vm.attr('message').should.equal('This is the counter-example component');
-  });
-});
