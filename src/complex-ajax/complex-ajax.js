3d538c72fa7fce73a08379b1b3fbe55671662672
Paul Tichonczuk <pault@tracer.ca>
2017-04-26 12:42:04 -0400

    still can't get this version going

--- src/complex-ajax/complex-ajax.js
+++ src/complex-ajax/complex-ajax.js
@@ -1,6 +1,5 @@
 import Component from "can-component";
 import CanMap from "can-map";
-import can from 'can/util/deferred';
 import "can-map-define";
 import './complex-ajax.less!';
 import view from './complex-ajax.stache!';
@@ -10,22 +9,22 @@ export const ViewModel = CanMap.extend({
   define: {
     stuff: {
 			value() {
-				var def = can.Deferred();
-				var response = [];
-				var counter = 2;
-					ajax("/api/complex-ajax1").then(
-						(result) => {
-							response.push(result);
-							counter--;
-							if (counter == 0) def.resolve(response);
-					});
-					ajax("/api/complex-ajax2").then(
-						(result) => {
-							response.push(result);
-							counter--;
-							if (counter == 0) def.resolve(response);
-					});
-				return def;
+				var promise = new Promise((resolve, reject) => {
+					var response = [];
+					var counter = 2;
+						ajax("/api/complex-ajax1").then(
+							(result) => {
+								response.push(result);
+								counter--;
+								if (counter == 0) resolve(response);
+						});
+						ajax("/api/complex-ajax2").then(
+							(result) => {
+								response.push(result);
+								counter--;
+								if (counter == 0) resolve(response);
+						});
+				});
 			}
     }
   }
