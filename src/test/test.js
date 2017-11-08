34f502ad46c69ca1b6807cfc54f905e6a523ca94
= <pault@DESKTOP-GPBCHOV.localdomain>
2017-11-08 16:57:44 -0500

    removing redundant ajax examples adding chrome testing setup

--- src/test/test.js
+++ src/test/test.js
@@ -5,12 +5,6 @@ import 'donejs-example/test/functional';
 
 import 'donejs-example/simple-ajax/simple-ajax_test';
 
-import 'donejs-example/modify-ajax/modify-ajax_test';
-
-import 'donejs-example/modify-ajax2/modify-ajax2_test';
-
 import 'donejs-example/simple-form/simple-form_test';
-
-import 'donejs-example/complex-ajax/complex-ajax_test';
 
 import 'donejs-example/live-binding/live-binding_test';
